e5bde3d2153e5a974c7f904a03982de832b14315
Jeremy Brien <brienjl@gmail.com>
2019-02-16 07:17:32 -0800

    rolled back to nodejs v8.12.0 and reinstalled dependencies

--- wdio.conf.js
+++ wdio.conf.js
@@ -111,7 +111,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],//
+    services: ['selenium-standalone'],
+    //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
@@ -123,7 +124,8 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    // reporters: ['dot'],
+    reporters: ['dot'],
+    
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
