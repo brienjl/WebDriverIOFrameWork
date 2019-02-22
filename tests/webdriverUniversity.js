6195485f240fd56d2cdde5cc09ae79be75f20227
Jeremy Brien <brienjl@gmail.com>
2019-02-22 14:38:38 -0800

    Updated webdriver tc

--- tests/webdriverUniversity.js
+++ tests/webdriverUniversity.js
@@ -7,19 +7,35 @@ describe("Verify whether webdriveruniversity link on homepage works correctly", 
             browser.url('/');
             var title = browser.getTitle();
             expect(title).to.equal('WebDriverUniversity.com');
-            title.should.equal('WebDriverUniversity.com');
             console.log('Title is: ' + title);
+
             browser.click("#contact-us");
-            browser.pause(3000);
+            var tabIds = browser.getTabIds();
+            console.log(tabIds);
+            browser.switchTab(tabIds[1]);
+
+            var title2 = browser.getTitle();
+            expect(title2).to.equal('WebDriver | Contact Us');
+
+            var url = browser.getUrl();
+            expect(url).to.include('Contact-Us', 'URL Mismatch');
+            browser.close();
         });
 
         it("Check that the login button opens the login portal page", function(done) {
             browser.url('/');
-            browser.click('#login-portal');
             var title = browser.getTitle();
-            expect(title).to.equal('WebDriverUniversity.com');
             title.should.equal('WebDriverUniversity.com');
             console.log('Title is: ' + title);
-            browser.pause(3000);
+    
+            browser.click('#login-portal');
+            var tabIds = browser.getTabIds();
+            browser.switchTab(tabIds[1]);
+    
+            var title2 = browser.getTitle();
+            expect(title2).to.equal('WebDriver | Login Portal');
+    
+            var url = browser.getUrl();
+            expect(url).to.include('Login-Portal', 'URL Mismatch');
         });
 });
